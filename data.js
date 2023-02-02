519046a20dafb52e7706cdca0c3895aff080efda
hibahaouari <hibahaouari4@gmail.com>
2023-02-02 09:25:11 +0100

    portrait-chinois

---
+++ data.js
@@ -0,0 +1,47 @@
+
+
+
+
+var test = [
+   
+   
+   
+    {analogie: 'une fleur',
+    valeurAnalogie:'le jasmin',
+    justificatifAnalogie:"Je suis  connu pour ma délicatesse et mon élégance, comme le jasmin qui est une fleur très parfumée et délicate. Je me sens romantique et sensuelle, comme le jasmin qui est souvent associé à l'amour et à la passion. Mon choix pour cette fleur peut également exprimer l'idée que j’apprécie les odeurs naturelles et préfère les fleurs qui dégagent un parfum agréable. Ça peut également expliquer le fait que j’aime les fleurs exotiques . ",
+    image: 'fleur.png'},
+    
+    {analogie: 'un endroit',
+    valeurAnalogie:'la mer',
+    justificatifAnalogie:"Je me  sens libre et indépendante, comme la mer qui est grande et ouverte. je me sens apaisée et détendue en présence de l'eau, comme on peut se sentir en bord de mer. La mer dans ses deux états me ressemble , j’ai une certaine force et puissance, comme la mer qui peut être calme et paisible mais aussi agitée et déchaînée. ",
+    image: 'mer.png'},
+
+    {analogie: 'animal',
+    valeurAnalogie:'un chien',
+    justificatifAnalogie:"je me sens fidèle et loyal, comme un chien qui est connu pour être un animal très fidèle. Il me semble que j’ai un caractère protecteur et attentif, comme un chien qui est souvent utilisé pour protéger les propriétés ou les personnes. J'apprécie aussi la compagnie et l'affection, comme les chiens qui sont des animaux très sociables et qui aiment être en présence de leur propriétaire. "
+    ,
+    image: 'chien.png'}, 
+
+    {analogie: 'un objet',
+    valeurAnalogie:'un livre',
+    justificatifAnalogie:"je suis intelligente et curieuse, comme un livre qui contient de l'information et des connaissances. Le livre me ressemble car on est tous les deux  riche d'imagination et d'émotions, comme le livre nous  emmène dans des mondes et des histoires différentes. J’apprécie bien les mots et les histoires, comme un livre qui est composé de mots et de phrases.",
+    image: 'livre.png'},
+
+    {analogie: 'un artiste',
+    valeurAnalogie:'un pianiste',
+    justificatifAnalogie:"Je me sens passionné de la  musique classique et j’apprécie trop les instruments à cordes. un pianiste est créatif et capable d'exprimer des émotions à travers la musique. La précision et la technique dans ce métier est fabuleuse , comme le pianiste doit maîtriser un grand nombre de nuances et de styles pour jouer du piano.",
+    image: 'piano.png'},
+
+    {analogie: 'une couleur',
+    valeurAnalogie:'le bleu ',
+    justificatifAnalogie:"Je me sens calme et apaisée, comme la couleur bleue qui est souvent associée à la sérénité. Je me sens profonde et réfléchie, comme la couleur bleue qui est souvent associée à la profondeur de l'océan. j’apprécie la simplicité et la pureté, comme la couleur bleue qui est une couleur primaire.",
+    image: 'couleur.png'},
+
+    {analogie: 'une activité',
+    valeurAnalogie:'la danse',
+    justificatifAnalogie:"Je me sens libre et expressive, comme la danse qui permet de bouger son corps et d'exprimer des émotions. Je suis créative et passionnée, comme la danse qui peut être utilisée pour créer des chorégraphies et des performances. j’apprécie les activités physiques et je préfère les activités qui impliquent le mouvement. ",
+    image: 'danse.png'}
+
+    
+
+  ];
---
+++ script.js
@@ -0,0 +1,101 @@
+
+
+// code pour afficher a droite de la page une previsualisation de ce que l'internaut a proposé
+
+
+ var button = document.querySelector('#envoi'); //   Obtenir référence au boutton de formulaire
+
+  
+ button.addEventListener('click', ()=>{ //ajouter un detecteur d'evenement pour le click sur le boutton envoyé
+
+   var ana = document.querySelector('#analogie').value; //  Obtenir la valeur saise dans le champs si j'etais  de formulaire
+   var valana = document.querySelector('#valeurAnalogie').value;//  Obtenir la valeur saise dans le champs je serais  de formulaire
+   var just = document.querySelector('#justificationAnalogie').value;//  Obtenir la valeur saise dans le champs je serais  de formulaire
+   var visuel = document.querySelector('#visual') //   Obtenir référence a la section dont le id est  visual et l'afecter a visuel
+
+  
+   var anavis = document.createElement('h1'); //  creer un nouveau element h1 (balise) pour l'analogie et la valeur analogie
+   anavis.textContent = "si j'etais "+ana+" je serais "+valana; // ajouter la valeur anavis a cette balise
+   visual.appendChild(anavis); // ajouter l'element anavis comme enfant a visual
+   
+
+   var justif = document.createElement('p');  //  creer un nouveau element p (balise) pour la justification de l'analogie
+   justif.textContent = just ; // ajouter la valeur just a cette balise
+   visual.appendChild(justif);// ajouter l'element justif comme enfant a visual
+
+  //recupre l'image proposé par l'internaute
+
+   var image = form.querySelector("input[type='file']");  //   Obtenir référence a l input dont le type est file et l'affecter a image
+   if (image.files && image.files[0]) {// test pour verifier si l'internaute a bien choisie une image
+    var img = document.createElement("img"); //creer un nouveau element img pour l'image
+    img.src = URL.createObjectURL(image.files[0]); //lier le lien de l'image selectionné a la balise
+    image.style
+    visual.appendChild(img);// ajouter l'element img comme enfant a visual
+  }
+   
+ });
+
+
+ 
+// code chargement de données pour les analogies
+
+var contenu = document.querySelector('#contenu'); //  Obtenir référence au bloc main
+
+test.forEach(function creerSection(test) {
+
+
+var sec = document.createElement('section');
+  var right = document.createElement('div');
+  
+  right.setAttribute('class','columnright');
+  var left = document.createElement('div');
+  left.setAttribute('class','columnleft');
+  var tiltle = document.createElement('h1');
+  tiltle.textContent="Si j'étais " + test.analogie+ ", je serais "+ test.valeurAnalogie;
+  var jus = document.createElement('p');
+  jus.textContent= test.justificatifAnalogie
+
+  left.setAttribute('class','columnright');
+  
+      // la division droite pour l'image
+    left.setAttribute('class','columnleft');
+    
+    ima=document.createElement('img');
+    ima.setAttribute('id',"ima")
+    ima.src = '/image/'+test.image;
+    
+
+
+
+
+
+    contenu.appendChild(sec);
+    sec.appendChild(right);
+    right.appendChild(tiltle);
+    right.appendChild(jus);
+    sec.appendChild(left);
+    left.appendChild(ima)
+
+
+  console.log("Si j'étais " + test.analogie+ " je serais "+ test.valeurAnalogie);
+  console.log(jus)
+})
+
+
+
+
+
+
+
+
+
+/*console.log(test);
+
+test.forEach(function affichtitre(test) {
+  console.log("Si j'étais " + test.analogie+ " je serais "+ test.valeurAnalogie);
+
+})
+
+*/
+
+
